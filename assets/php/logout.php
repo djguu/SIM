eb322613c316b2e832f406496a276caea9a14c18
Goncalo Correia <goncalopcorreia@hotmail.com>
2020-01-29 00:57:25 +0000

    changed url

--- assets/php/logout.php
+++ assets/php/logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); //to ensure you are using same session
 session_destroy(); //destroy the session
-header("location: ".$_SERVER['DOCUMENT_ROOT']."/assets/pages/login.php"); //to redirect back to "index.php" after logging out
+$url = $_SERVER['DOCUMENT_ROOT']."/assets/pages/login.php";
+header("location: ".$url); //to redirect back to "index.php" after logging out
 exit();
